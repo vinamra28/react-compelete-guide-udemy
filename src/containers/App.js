6fff8de810f46e2e353e0c9830c7f11dd08c1dea
vinamra28 <vinjain@redhat.com>
2020-09-27 16:31:06 +0530

    Setting states correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
     otherState: "Some other state",
     showPersons: false,
     showCockpit: true,
+    changeCounter: 0,
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -47,8 +48,11 @@ class App extends React.Component {
     person.name = event.target.value;
     const persons = [...this.state.persons];
     persons[personIndex] = person;
-    this.setState({
-      persons: persons,
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1,
+      };
     });
   };
 
